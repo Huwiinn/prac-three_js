b276edcf7c9d715ded5744e71d66d47536fd5fc0
hwuiinn <gnldls8406@gmail.com>
2024-11-30 18:29:28 +0900

    feat: 캔버스 배경 색, 투명도 설정 - 배경 색 적용 우선순위에 대해 학습

--- basic_fin/src/main.js
+++ basic_fin/src/main.js
@@ -1,5 +1,7 @@
-import example from "./myPracJs/ex01";
-import browserResize from "./myPracJs/ex02";
+// import example from "./myPracJs/ex01";
+// import browserResize from "./myPracJs/ex02";
+import BackgroundColorAndOpacity from "./myPracJs/ex03";
 
-example();
-browserResize();
+// example();
+// browserResize();
+BackgroundColorAndOpacity();
