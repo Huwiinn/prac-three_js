46fcd4f9132ba3ae1ee5d81785f2cde17cf853d4
hwuiinn <gnldls8406@gmail.com>
2024-11-30 17:37:59 +0900

    feat: Orthographic Camera - Zoom in, out 할 때에는 camera의 zoom 속성을 조절 - zoom 속성을 변경하고 반드시 updateProjectionMatrix 메서드를 실행해줘야 함

--- basic_fin/src/main.js
+++ basic_fin/src/main.js
@@ -34,6 +34,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 
 // ✅ Camera
+// PerspectiveCamera와 Orthographic Camera가 있는데, 보통 PerspectiveCamera를 사용한다.
 // PerspectiveCamera( fov[시야각] : Number, aspect[가로-세로 비율] : Number, near[얼마나 가까운 거리에서 오브젝트가 보이게 할 것인지] : Number, far[얼마나 멀리있는 거리에서 오브젝트가 보이지 않게 할 것인지] : Number )
 const camera = new THREE.PerspectiveCamera(
   75, // field of view, 시야각
@@ -42,11 +43,36 @@ const camera = new THREE.PerspectiveCamera(
   1000 // far
 );
 
-// Tip. 거리 단위는 내가 어떤 것을 만드는지에 따라 상대적임. m(미터) 단위 기준으로 잡았다고 가정하고 cm나 inch 등을 단위 변환하여 적용하면 작업하기 쉬움
-// ** 카메라 포지션 => 오른쪽으로 간다 ? 사물이 왼쪽에 있는 것 처럼 보인다
+// Orthographic Camera(직교 카메라)
+// 렌더링된 이미지에서 객체의 크기는 카메라와의 거리에 관계없이 일정하게 유지됩니다.
+// 이 기능은 무엇보다도 2D 장면과 UI 요소를 렌더링하는 데 유용합니다.
+// OrthographicCamera( left : Number, right : Number, top : Number, bottom : Number, near : Number, far : Number )
+// left — 카메라 절두체 좌평면.
+// right — 카메라 절두체 우평면.
+// top — 카메라 절두체 상평면.
+// bottom — 카메라 절두체 하평면.
+// near — 카메라 절두체 근평면.
+// far — 카메라 절두체 원평면.
+
+// const camera = new THREE.OrthographicCamera(
+//   -(window.innerWidth / window.innerHeight), // left
+//   window.innerWidth / window.innerHeight, // right
+//   1, // top
+//   -1, // bottom
+//   0.1, // top
+//   1000 // bottom
+// );
+
+// // Tip. 거리 단위는 내가 어떤 것을 만드는지에 따라 상대적임. m(미터) 단위 기준으로 잡았다고 가정하고 cm나 inch 등을 단위 변환하여 적용하면 작업하기 쉬움
+// // ** 카메라 포지션 => 오른쪽으로 간다 ? 사물이 왼쪽에 있는 것 처럼 보인다
+// // Zoom out, in 기능은 z 속성으로 카메라를 위치시키는 것이 아니라 zoom과 updateProjectionMatrix 메서드를 사용하여 구현해야한다.
+// // z 속성으로 카메라 위치를 변경하여 줌아웃을 구현하게되면 사물이 이상하게 보이는 경우가 있기 때문임.
 camera.position.x = 2; // 양수일 때, (사물이) 왼쪽으로 위치함. - 카메라는 오른쪽으로 이동
 camera.position.y = 2; // 양수일 때, (사물이) 아래로 내려감. - 카메라는 위로 이동
 camera.position.z = 6; // 양수일 때, (사물이) 앞으로 위치함. - 카메라는 사물 가까이 이동
+camera.lookAt(0, 0, 0);
+camera.zoom = 2.5;
+camera.updateProjectionMatrix();
 scene.add(camera);
 
 // 카메라 위치 설정을 하지 않으면 default location value는 [ 0,0,0 ] 이다.
