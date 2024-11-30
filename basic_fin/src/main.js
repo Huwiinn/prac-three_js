1aaea1bcf322867731bd5085464a93224967295b
hwuiinn <gnldls8406@gmail.com>
2024-11-30 16:51:27 +0900

    feat: scene 만들기 - Scene 생성 - PerspectiveCamera (원근 카메라) 생성 - 카메라 포지션 설정

--- basic_fin/src/main.js
+++ basic_fin/src/main.js
@@ -11,6 +11,7 @@
 // example();
 
 import * as THREE from "three";
+import { Scene } from "three";
 
 // 1. renderer를 먼저 생성해줘야 한다.
 // 2. renderer 사이즈를 세팅해야한다.
@@ -25,3 +26,19 @@ import * as THREE from "three";
 const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+
+// PerspectiveCamera( fov[시야각] : Number, aspect[가로-세로 비율] : Number, near[얼마나 가까운 거리에서 오브젝트가 보이게 할 것인지] : Number, far[얼마나 멀리있는 거리에서 오브젝트가 보이지 않게 할 것인지] : Number )
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view, 시야각
+  window.innerWidth / window.innerHeight, // aspect, 종횡비
+  0.1, // near, 보통 near는 0.1로 설정함.
+  1000 // far
+);
+
+// Tip. 거리 단위는 내가 어떤 것을 만드는지에 따라 상대적임. m(미터) 단위 기준으로 잡았다고 가정하고 cm나 inch 등을 단위 변환하여 적용하면 작업하기 쉬움
+camera.position.z = 5; // 양수일 때, 앞으로 위치함.
+scene.add(camera);
+
+// 카메라 위치 설정을 하지 않으면 default location value는 [ 0,0,0 ] 이다.
